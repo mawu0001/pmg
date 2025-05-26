8e22cc63dc7bcb3123523f1e8a225b760915db1c
Dimi <diba0001@stud.kea.dk>
2025-05-26 09:26:17 +0200

    Edited: added re squares to the nav-links to align to PMG design

--- src/app/components/navigation.jsx
+++ src/app/components/navigation.jsx
@@ -20,11 +20,11 @@ const Navbar = () => {
         </Link>
         <ul className="flex space-x-6 text-sm font-medium text-gray-700">
           {/* lokale links */}
-        <li><Link href="#inspiration" className="hover:text-redColor">INSPIRATION</Link></li> 
-          <li><Link href="#kontakt" className="hover:text-redColor">KONTAKT</Link></li>
+          <li><Link href="#inspiration" className="hover:text-redColor flex items-center before:content-[''] before:inline-block before:w-2 before:h-2 before:bg-red-500 before:mr-2">INSPIRATION</Link></li> 
+          <li><Link href="#kontakt" className="hover:text-redColor flex items-center before:content-[''] before:inline-block before:w-2 before:h-2 before:bg-red-500 before:mr-2">KONTAKT</Link></li>
           {/* pmg-website links (eksternt til wordpress site) */}
-          <li><Link href="https://piccolomedia.dk/piccolo-prisen-finalister/" className="hover:text-redColor">PICCOLO-PRISEN</Link></li>
-          <li><Link href="https://piccolomedia.dk/#ompmg" className="hover:text-redColor">OM PMG</Link></li>
+          <li><Link href="https://piccolomedia.dk/piccolo-prisen-finalister/" className="hover:text-redColor flex items-center before:content-[''] before:inline-block before:w-2 before:h-2 before:bg-red-500 before:mr-2">PICCOLO-PRISEN</Link></li>
+          <li><Link href="https://piccolomedia.dk/#ompmg" className="hover:text-redColor flex items-center before:content-[''] before:inline-block before:w-2 before:h-2 before:bg-red-500 before:mr-2">OM PMG</Link></li>
         </ul>
       </div>
     </nav>
