b533e232e4d7b36340a7c5fd68a6a8cbc3b444b3
Marie Wulff <mawu0001@stud.kea.dk>
2025-06-10 13:52:11 +0200

    margin added

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold mb-2 text-black">forside her</h1>
+      <h1 className="text-3xl font-bold mb-2 my-0.5 text-black">forside her</h1>
     </>
   );
 }
