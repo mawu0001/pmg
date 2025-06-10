c7b2a10c007b309b89947af4146cea599fca7463
Marie Wulff <mawu0001@stud.kea.dk>
2025-06-10 15:27:17 +0200

    image udkommenteret

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,20 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold mb-2 my-0.5 text-black">forside her</h1>
+      {/* <Image
+        src="/images/homepage-indhold/Annoncoer_topslider.jpg"
+        alt="Eksempel på magasinannonce"
+        width={1300}
+        height={100}
+        priority
+      /> */}
+      <section>
+        <h1 className="text-3xl font-bold mb-2 my-0.5 text-black">
+          forside her
+        </h1>
+      </section>
     </>
   );
 }
